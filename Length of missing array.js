2236fd6e329cc872e4c7c741cfce4200da52a812
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-03-19 16:14:47 +0300

    feat: finish

--- Length of missing array.js
+++ Length of missing array.js
@@ -1,19 +1,32 @@
 function getLengthOfMissingArray(arrayOfArrays) {
-	 
-	let arr = arrayOfArrays.map(a => a.length ).sort()
-if (arr.includes(0)) {
+	const arr = (arrayOfArrays || [])
+	.map(a => a ? a.length : 0)
+	.sort((a, b) => a - b)
+	
+	console.log(arr);
+	if (arrayOfArrays == null || arrayOfArrays.length == 0){
+    return 0;
+	}
+	if (arr.includes(0)) {
 	return 0
-}
+}	
 	 for (let i = 0; i < arr.length; i++){
 	 if ( arr[i] + 1 !== arr[i + 1] ){
 		 return arr[i] + 1;
 	 }
-	}
 	
 	}
+return 0
+	}
 	   
- console.log(getLengthOfMissingArray([ [], [ 1, 2 ], [ 4, 5, 1, 1 ], [ 5, 6, 7, 8, 9 ]]));
- console.log(getLengthOfMissingArray([[]]));
+ console.log(getLengthOfMissingArray([[[4, 4, 2, 4],
+	[2, 2, 4, 1, 3, 2],
+	[4, 2, 3, 4, 3, 3, 3],
+	[3, 3, 3, 0, 3, 3, 0, 0],
+	[3, 2, 0, 3, 4, 0, 2, 3, 3],
+	[1, 1, 1, 2, 4, 0, 3, 2, 0, 2],
+	[4, 3, 1, 4, 2, 0, 1, 1, 3, 3, 2]]]));
+ console.log(getLengthOfMissingArray([]));
 
 
 
