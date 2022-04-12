c4b850f293e4c5edf2536cdfe7e4566eb8f60cd1
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-04-13 00:23:11 +0300

    add Adding Arrays

---
+++ Adding Arrays.js
@@ -0,0 +1,13 @@
+function arrAdder(arr) {
+	let str =''
+	for (let i = 0; i < arr[0].length; i++) {
+		for (let j = 0; j < arr.length; j++) {
+			str+=arr[j][i]
+		}
+		str+=' '
+		
+return str.trim()
+
+
+}
+arrAdder(arr)
