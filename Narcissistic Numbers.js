64af016bd4ed54bdd2c13b3a02922609096f7daf
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-04-21 00:17:23 +0300

    add alternative

--- Narcissistic Numbers.js
+++ Narcissistic Numbers.js
@@ -1,14 +1,15 @@
 function isNarcissistic(n){
-	
 	let sum = 0
 	let a = n.toString()
-	let b =a.split('')
-	let c = b.map(el =>el**b.length)
-	for(let i = 0; i < c.length; i++){
-		sum = sum + c[i]
+	let b =a.split('').map(el =>el**a.length)
+	for(let i = 0; i < b.length; i++){
+		sum = sum + b[i]
 	}
 	if( sum !== n){
 		return false
 	} return true
 	
 }
+function isNarcissistic(n) {
+	return n === n.toString().split('').reduce((res, num, index, arr) => res += Math.pow(num, arr.length), 0)
+ }
