84bc15ad4a3efd26a0877f83ebb775288cf6507d
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-04-20 23:05:08 +0300

    correct code

--- You Got Change.js
+++ You Got Change.js
@@ -1,18 +1,4 @@
-function giveChange(amount) {
-  let arr = []
-  let a = Math.trunc(amount / 100)
-  arr.unshift(a)
-  let b = Math.trunc((amount - a*100) / 50)
-  arr.unshift(b)
-  let c = Math.trunc(((amount - a*100)-b*50)/20)
-  arr.unshift(c)
-  let d = Math.trunc(((amount - a*100)-b*50 - c*20)/10)
-  arr.unshift(d)
-  let f = Math.trunc(((amount - a*100)-b*50 - c*20 - d*10)/5)
-  arr.unshift(f)
-  let g = Math.trunc(((amount - a*100)-b*50 - c*20 - d*10 - f*5)/1)
-  arr.unshift(g)
-  return arr;
-}
+const giveChange = amount => [100, 50, 20, 10, 5, 1].map(a => 
+	([a, amount] = [Math.floor(amount/a), amount % a])[0]).reverse();
 
 giveChange(2170);
