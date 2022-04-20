c2a76361e5720394393b448d646f4c1a6fa80cb5
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-04-21 00:14:38 +0300

    correct Narcissistic Numbers

--- Narcissistic Numbers.js
+++ Narcissistic Numbers.js
@@ -12,21 +12,3 @@ function isNarcissistic(n){
 	} return true
 	
 }
- console.log(isNarcissistic(153));
- console.log(isNarcissistic(1634));
-
-/* function isNarcissistic(n){
-  
-	let sum = 0
-	let a = n.toString()
-	let b =a.split('').map(el =>el**3)
-	for(let i = 0; i < b.length; i++){
-		sum = sum + b[i]
-	}
-	console.log(sum);
-	console.log(typeof(sum));
-	if( sum === n){
-		return true
-	} return false
-}
-console.log(isNarcissistic(153)); */
