aeeb8ccaa6f0a3df6e43829f5738138df38899b9
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-06-07 23:05:27 +0300

    finish

--- Coprime Validator.js
+++ Coprime Validator.js
@@ -1,5 +1,15 @@
 function isCoprime(x, y) {
-	// your code here
+	let arr = []
+	let arr1 = []
+	for (let i = 1; i <= x; i++) {
+		if( x % i === 0){
+			arr.push(i)
+		}
+	}
+	for (let j = 1; j <= y; j++) {
+		if( y % j === 0){
+			arr1.push(j)
+		}
+	}
+return arr.filter(el => arr1.indexOf(el) >0).length === 0 ? true : false 
  }
-
- console.log(isCoprime(20,27));
