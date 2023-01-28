0750980ebca0ec5762a7af78736567b2080cab52
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-01-28 14:50:27 +0300

    add palindrome

---
+++ Palindrome for your Dome.js
@@ -0,0 +1,15 @@
+function palindrome(string) {
+	//const newString = string.replace(/[^0-9aA-zZ.]/g, '').replace(/[\s_,.,%]/g,'').toLowerCase()
+	const newString = string.replace(/[^A-Za-z0-9]/,"").toLowerCase()
+	const polindrom = newString.split("").reverse().join("").toLowerCase()
+	console.log(newString);
+	console.log(polindrom);
+if(string = ''){
+	return true
+} else if (newString === polindrom ) {
+return true
+} else return false
+}
+
+ console.log(palindrome("h635VxADEuNvvNu'EDAx?V5,3.6h;"));//true 
+ console.log(palindrome("Under_scores; Serocsrednu"))
